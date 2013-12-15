ec177b65b18d68b9c27edeccc7804eb57abb5260
Evan Goodberry <egoodberry@gmail.com>
2013-12-15 08:17:19 -0500

    Initial Commit

---
+++ ForeignNumbers/EGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EGAppDelegate.h
+//  ForeignNumbers
+//
+//  Created by Evan Goodberry on 12/15/13.
+//  Copyright (c) 2013 Evan Goodberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
