8a37446ba0a92c7b9debba473a0f2ec4c3b7e559
Evan Goodberry <egoodberry@gmail.com>
2013-12-16 21:25:15 -0500

    Clean up.

--- ForeignNumbers/EGMainViewController.m
+++ ForeignNumbers/EGMainViewController.m
@@ -24,15 +24,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIToolbar* numberToolbar = [[UIToolbar alloc] initWithFrame: CGRectMake(0, 0, 320, 50)];
-    numberToolbar.items = [NSArray arrayWithObjects:[
-    [UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelNumberPad)],
-    [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
-    [[UIBarButtonItem alloc] initWithTitle: @"Submit" style: UIBarButtonSystemItemDone target:self action:@selector(submitAnswer)],
-    nil
-    ];
-    [numberToolbar sizeToFit];
-    self.answerInput.inputAccessoryView = numberToolbar;
+    [self buildNumberToolbar];
 }
 
 - (void)cancelNumberPad {
@@ -46,7 +38,6 @@
     self.giveUpButton.enabled = true;
     self.answerInput.enabled = true;
 }
-
 
 - (IBAction)repeatPressed {
     [self speakCurrentAnswer];
@@ -99,6 +90,18 @@
     else {
         [self resetForm];
     }
+}
+
+- (void)buildNumberToolbar {
+    UIToolbar* numberToolbar = [[UIToolbar alloc] initWithFrame: CGRectMake(0, 0, 320, 50)];
+    numberToolbar.items = [NSArray arrayWithObjects:[
+                                                     [UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelNumberPad)],
+                           [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                           [[UIBarButtonItem alloc] initWithTitle: @"Submit" style: UIBarButtonSystemItemDone target:self action:@selector(submitAnswer)],
+                           nil
+                           ];
+    [numberToolbar sizeToFit];
+    self.answerInput.inputAccessoryView = numberToolbar;
 }
 
 - (void)speakNewNumber {
